1cbae1614bd919a624425e8592bb97766e81c100
Rubén <ruben.sr.apple@gmail.com>
2024-11-13 21:51:37 +0100

    Initial Commit

---
+++ PS5GamesApp/PS5GamesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PS5GamesAppApp.swift
+//  PS5GamesApp
+//
+//  Created by Rubén Segura Romo on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PS5GamesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
